547acb0e4c6cb922223e5114b144ba832a897ecb
Simon Bishop <simonbishop@Simons-MacBook-Pro.local>
2017-03-09 15:59:31 +0200

    updated for node 7.7

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function check(port, host) {
     client = new net.Socket();
     client.once('connect', onConnectCb);
     client.once('error', onErrorCb);
-    client.connect({port: opts.port, host: opts.host});
+    client.connect({port: opts.port, host: opts.host}, function(){});
 
     return deferred.promise;
 }
